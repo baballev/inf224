b6d3a7f31bbe36be54204a10cdc9c2cc622ecd0b
Vincent Josse <vincent.josse@telecom-paris.fr>
2020-12-08 19:31:44 +0100

    Tested step 4, next time go on with step 5

--- main.cpp
+++ main.cpp
@@ -2,7 +2,12 @@
 // main.cpp
 // Created on 21/10/2018
 //
+#ifndef MEDIA
+#define MEDIA
 #include "Media.h"
+#include <iostream>
+#endif
+
 #include "Image.h"
 #include "Video.h"
 
@@ -15,7 +20,9 @@ int main(int argc, const char* argv[])
     //Media * media = new Media();
     //media->print(std::cout);
     Image *img = new Image(); 
-
+    img->setHeight(145);
+    img->setWidth(255);
+    img->print(std::cout);
 
     return 0;
 }
